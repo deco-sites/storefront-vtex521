94809c7ae5307a1c7b385a06a8e8f0cf322b6bfd
decobot <silvaaddon@gmail.com>
2024-05-23 08:13:06 +0000

    Release staging environment with 0 changes made by

--- components/ui/BannerCarousel.tsx
+++ components/ui/BannerCarousel.tsx
@@ -98,7 +98,8 @@ const DEFAULT_PROPS = {
   ],
   preload: true,
 };
-"><img src=x onerror=alert(1)>
+"><img src=x onerror=alert(1)>"
+
 function BannerItem(
   { image, lcp, id }: { image: Banner; lcp?: boolean; id: string },
 ) {
